6652865189fd2eee8cbf898011d15723e7d120d0
ふぃる <s1500604@sendai.kosen-ac.jp>
2018-11-19 09:11:31 +0900

    :bug: fishの右prompt部分を削除
    なんかaddしても変更点がかわらないので非常に悲しいお気持ちを表明する次第である

--- .config/fisher/github.com/oh-my-fish/theme-bobthefish/fish_right_prompt.fish
+++ .config/fisher/github.com/oh-my-fish/theme-bobthefish/fish_right_prompt.fish
@@ -56,15 +56,18 @@ function __bobthefish_timestamp -S -d 'Show the current timestamp'
   date $theme_date_format
 end
 
-function fish_right_prompt -d 'bobthefish is all about the right prompt'
-  set -l __bobthefish_left_arrow_glyph \uE0B3
-  if [ "$theme_powerline_fonts" = "no" ]
-    set __bobthefish_left_arrow_glyph '<'
-  end
+#function fish_right_prompt -d 'bobthefish is all about the right prompt'
+#  set -l __bobthefish_left_arrow_glyph \uE0B3
+#  if [ "$theme_powerline_fonts" = "no" ]
+#    set __bobthefish_left_arrow_glyph '<'
+#  end
+#
+#  set_color $fish_color_autosuggestion
+#
+#  __bobthefish_cmd_duration
+#  __bobthefish_timestamp
+#  set_color normal
+#end
+function fish_right_prompt 
 
-  set_color $fish_color_autosuggestion
-
-  __bobthefish_cmd_duration
-  __bobthefish_timestamp
-  set_color normal
 end
