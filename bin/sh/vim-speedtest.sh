00c997361349ed24282158467afad15e972200c8
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2018-12-10 22:42:39 +0900

    :bug: :trollface: spが引数有効になってなかった

--- bin/sh/vim-speedtest.sh
+++ bin/sh/vim-speedtest.sh
@@ -2,8 +2,21 @@
 
 function vim-speed-test()
 {
-  file_name=$(date '+%m%d-%H-%M-%S')
-  vi --startuptime ~/dotfiles/vim/logs/$file_name.log 
-  vi ~/dotfiles/vim/logs/$file_name.log
+  # echo $#
+  if [ $# -eq 0 ]; then
+    file_name=$(date '+%m%d-%H-%M-%S')
+    nvim --startuptime ~/dotfiles/vim/logs/$file_name.log
+    nvim ~/dotfiles/vim/logs/$file_name.log
+    exit 1
+  fi
+  # file_name=`echo $1 | awk '{print substr($0, index($0, "/"))}'`
+  # file_name=`echo $file_name |cut -c 2-`
+  temp=$1
+  file_name=${temp##*/}
+  file_name=$(date '+%m%d-%H-%M-%S')'___'$file_name'___'
+  echo $file_name
+  nvim $1 --startuptime ~/dotfiles/vim/logs/$file_name.log
+  nvim ~/dotfiles/vim/logs/$file_name.log
+  exit 0
 }
-vim-speed-test
+vim-speed-test $1
