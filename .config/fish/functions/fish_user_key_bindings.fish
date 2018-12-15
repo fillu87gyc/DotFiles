89184879f440e4dcff166df7a43792951bac081a
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2018-12-16 01:12:57 +0900

    :wrench: zで.gitに移動しないようにした close #31

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
   fzf_key_bindings_default
   fzf_key_bindings_ghq
+  bind \cy fzf_z
   bind \c] peco_select_ghq_repository  # 追加
   bind \co peco_z # Ctrl-[にバインドする
 end
