ee9cb2f353c3457956644e3cc280a850cc6c4462
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2019-01-13 18:41:11 +0900

    :tada: tmuxの見た目を更新 close #55

--- .tmux/right_prompt.sh
+++ .tmux/right_prompt.sh
@@ -4,7 +4,5 @@ btt="#(~/dotfiles/bin/sh/mybattery -ep)"
 btt_graph="#(~/dotfiles/.tmux/.show_btty_graph.sh)"
 wifi="#(wifi)"
 date="#(~/dotfiles/.tmux/.date_format4eng.sh)"
-# prompt="${btt} ️${btt_graph}| ${wifi} | ${date}"
-prompt=" ⚡️${btt}%${btt_graph}| 🌐 ${wifi} | ${date}"
-
+prompt="#[bg=colour234]#(~/dotfiles/.tmux/get_net_speed.sh)#[bg=colour237] ${btt}%${btt_graph}#[bg=colour239] #[bg=colour242] ${wifi} #[bg=colour244] ${date}"
 echo $prompt
